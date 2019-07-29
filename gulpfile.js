9056f3575f64aba895c7258748f5803e741b5d43
H. Turgut Uyar <uyar@tekir.org>
2019-07-29 16:57:41 +0300

    add cssmin gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,16 @@ gulp.task('css', function () {
     return gulp.src('./theme/sass/**/*.scss')
         .pipe(sass())
         .pipe(postcss([uncss({html: ['./_build/**/*.html']})]))
-        .pipe(gulp.dest('./theme/css'));
+        .pipe(gulp.dest('./theme/css/'));
 });
 
-gulp.task('build', gulp.series('css'));
+gulp.task('cssmin', function () {
+    const cssmin = require('gulp-cssmin');
+
+    return gulp.src('./theme/css/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('./assets/static/'));
+});
+
+gulp.task('build', gulp.series('css', 'cssmin'));
 gulp.task('default', gulp.series('build'));
