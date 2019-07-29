e883cb84688583d85c58ac47969cf41d8e15f2a5
H. Turgut Uyar <uyar@tekir.org>
2019-07-29 16:50:23 +0300

    add uncss to gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,12 @@ gulp.task('css', function () {
     const sass = require('gulp-sass');
     sass.compiler = require('node-sass');
 
+    const postcss = require('gulp-postcss');
+    const uncss = require('postcss-uncss');
+
     return gulp.src('./theme/sass/**/*.scss')
         .pipe(sass())
+        .pipe(postcss([uncss({html: ['./_build/**/*.html']})]))
         .pipe(gulp.dest('./theme/css'));
 });
 
