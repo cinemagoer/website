f085adb224f433ea5fd2dd939b916d67fc374779
H. Turgut Uyar <uyar@tekir.org>
2019-07-31 16:06:20 +0300

    separate css building into stages to make asseturl work properly
    [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,36 @@
 var gulp = require('gulp');
 
-gulp.task('css', function () {
+gulp.task('sass', function () {
     const sass = require('gulp-sass');
     sass.compiler = require('node-sass');
-
-    const postcss = require('gulp-postcss');
-    const uncss = require('postcss-uncss');
-
     return gulp.src('./theme/sass/**/*.scss')
         .pipe(sass())
-        .pipe(postcss([uncss({html: ['./_build/**/*.html']})]))
+        .pipe(gulp.dest('./assets/static/'));
+});
+
+gulp.task('uncss', function () {
+    const postcss = require('gulp-postcss');
+    const uncss = require('postcss-uncss');
+    return gulp.src('./assets/static/imdbpy.css')
+        .pipe(postcss([
+            uncss({
+                html: ['./_build/**/*.html']
+            })
+        ]))
         .pipe(gulp.dest('./theme/css/'));
 });
 
 gulp.task('cssmin', function () {
     const cssmin = require('gulp-cssmin');
-
-    return gulp.src('./theme/css/*.css')
+    return gulp.src('./theme/css/imdbpy.css')
         .pipe(cssmin())
         .pipe(gulp.dest('./assets/static/'));
 });
 
-gulp.task('build', gulp.series('css', 'cssmin'));
+gulp.task('build', gulp.series('sass'));
+gulp.task('after_build', gulp.series('uncss', 'cssmin'));
 gulp.task('watch', function () {
-    return gulp.watch('./theme/sass/**/*.scss', gulp.series('css', 'cssmin'));
+    return gulp.watch('./theme/sass/**/*.scss', gulp.series('build', 'after_build'));
 });
 
 gulp.task('default', gulp.series('build'));
